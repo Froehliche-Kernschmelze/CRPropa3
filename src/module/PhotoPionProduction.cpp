18cbfa7152f11e09ac1239075f63f385d4a91c4e
Mario Hörbe <mario.hoerbe@rub.de>
2019-01-11 17:19:13 +0100

    debug

--- src/module/PhotoPionProduction.cpp
+++ src/module/PhotoPionProduction.cpp
@@ -119,7 +119,7 @@ double PhotoPionProduction::nucleonMFP(double gamma, double z, bool onProton) co
         return std::numeric_limits<double>::max();
 
     double rate = interpolate2d(z, gamma, tabRedshifts, tabLorentz, tabRate);
-    std::cout <<  int(onProton) << " " << z << " " << gamma << " " << rate << " | ";
+    // std::cout <<  int(onProton) << " " << z << " " << gamma << " " << rate << " | ";
     // cosmological scaling
     rate *= pow(1 + z, 2);
 
@@ -209,7 +209,7 @@ void PhotoPionProduction::performInteraction(Candidate *candidate, bool onProton
     double outputEnergy[2000];
     int outPartID[2000];
     int nOutPart;
-    // std::cout << nature << " : " << Ein << " : " << eps << std::endl;
+    std::cout << int(onProton) << " : " << Ein << " : " << eps << std::endl;
     #pragma omp critical
     {
         sophiaevent_(nature, Ein, eps, outputEnergy, outPartID, nOutPart);
